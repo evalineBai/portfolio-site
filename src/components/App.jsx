f4327463f12de29d1c1cb999ba650b0a0ce5037f
Evaline <evalinebai@gmail.com>
2018-05-05 17:52:47 -0700

    Create grid structure for landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './Nav';
+import Landing from './Landing';
 import style from '../style/app.less';
 
 class App extends React.Component {
@@ -16,7 +17,9 @@ class App extends React.Component {
         <span className={style['col-1-10']}>
           <Nav />
         </span>
-        <span className={style['col-9-10']}>Content Box</span>
+        <span className={style['col-9-10']}>
+          <Landing />
+        </span>
       </div>
     );
   }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navlink } from 'react-router-dom';
+import style from '../style/landing.less';
+
+const Landing = () => (
+  <div className={style.landing}>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+  </div>
+);
+
+export default Landing;
