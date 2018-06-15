30eba23e4a9629b5b0958503dd82628d95400812
Evaline <evalinebai@gmail.com>
2018-06-14 17:13:00 -0700

    Refine about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,10 @@ import React from 'react';
 const About = () => (
   <div>
     <p>
-      I am a software developer and business professional, dedicated to finding technological solutions to public problems. I grew up in the great, flat, American Midwest and found myself enthralled by the intersections of economics and environmentalism while at the University of Chicago. My first post-grad job involved consulting fortune-100 companies on large-scale technology transformations. I picked up new technologies quickly and transitioned to software engineering.
+      I am a largely self-taught software developer and business professional, dedicated to finding technological solutions to public problems. I graduated from The University of Chicago in 2016. I then went into management consulting at Accenture, working on large-scale technology transformations for Fortune 100 companies. Today, I'm busy leveling up in blockchain development, contributing to open source projects, and trying to make the world a more amenable place in any small way.
     </p>
     <p>
-      I love creating things, coding, building decentralizaed applications and interesting UI.
+      I love creating, building applications, painting, and am constantly learning new things. My current tools of choice are Javascript, Python, React, Node.js, and the Ethereum stack. Let's get in touch!
     </p>
   </div>
 );
