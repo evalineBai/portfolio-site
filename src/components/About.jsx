5146316443609b3ce5a2c47c9e320bc1af5e1bc9
Evaline <evalinebai@gmail.com>
2018-06-15 17:29:41 -0700

    Add stylesheet for about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,16 @@
-// I love creating things. Coding and painting have always been extremely cathartic for me and I'm happy to be leading a life where I can mix creativity and structured logic in my day-to-day as a software developer.
-
-// I grew up in the great, flat, American Midwest and found myself enthralled by the intersections of economics and environmentalism while at the University of Chicago. My first post-grad job involved consulting fortune-100 companies on large-scale technology transformations. I picked up new technologies quickly and transitioned to software engineering.
-
 import React from 'react';
+import style from '../style/about.less';
 
 const About = () => (
   <div>
-    <p>
-      I am a largely self-taught software developer and business professional, dedicated to finding technological solutions to public problems. I graduated from The University of Chicago in 2016. I then went into management consulting at Accenture, working on large-scale technology transformations for Fortune 100 companies. Today, I'm busy leveling up in blockchain development, contributing to open source projects, and trying to make the world a more amenable place in any small way.
-    </p>
-    <p>
-      I love creating, building applications, painting, and am constantly learning new things. My current tools of choice are Javascript, Python, React, Node.js, and the Ethereum stack. Let's get in touch!
-    </p>
+    <div className={style.about}>
+      <p>
+        I am a software developer and business professional, interested in finding technological solutions to public problems. I graduated from The University of Chicago in 2016. I then went into management consulting at Accenture, working on large-scale technology transformations for Fortune 100 companies. Today, I'm busy leveling up in blockchain development, contributing to open source projects, and exploring beautiful Los Angeles.
+      </p>
+      <p>
+        I love creating, building applications, painting, and constantly learning new things. My current tools of choice are Javascript, Python, React, Node.js, and the Ethereum stack. Let's get in touch!
+      </p>
+    </div>
   </div>
 );
 
