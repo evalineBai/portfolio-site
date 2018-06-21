61a0a5e10f00e9591b1f861c86ddad158cfd96fb
Evaline <evalinebai@gmail.com>
2018-06-21 15:24:16 -0700

    Create layout for portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import style from '../style/portfolio.less';
 
 const Portfolio = () => (
-  <div>
+  <div className={style.portfolio}>
+    <div>
+      <h3>BlockBallot</h3>
+      <div className={style.images}>
+        <div className={style.one} />
+        <div className={style.two} />
+        <div className={style.three} />
+      </div>
+      <div>
+        <h5>A voting platform built on the Ethereum network</h5>
+      </div>
+    </div>
+    <hr/>
+    <div>
+      <h3>Meet Sean Mills!</h3>
+      <div className={style.images}>
+        <div className={style.one} />
+        <div className={style.two} />
+        <div className={style.three} />
+      </div>
+      <div>
+        <h5>A voting platform built on the Ethereum network</h5>
+      </div>
+    </div>
+    <hr />
+    <div>
+      <h3>Cartblanched</h3>
+      <div className={style.images}>
+        <div className={style.one} />
+        <div className={style.two} />
+        <div className={style.three} />
+      </div>
+      <div>
+        <h5>A voting platform built on the Ethereum network</h5>
+      </div>
+    </div>
+    <hr />
+    <div>
+      <h3>Locastore</h3>
+      <div className={style.images}>
+        <div className={style.one} />
+        <div className={style.two} />
+        <div className={style.three} />
+      </div>
+      <div>
+        <h5>A voting platform built on the Ethereum network</h5>
+      </div>
+    </div>
   </div>
 );
 
