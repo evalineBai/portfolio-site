3758b5ccabc82f52f6a98913bb5c42be0f5f21df
Evaline <evalinebai@gmail.com>
2018-06-21 19:23:13 -0700

    Create responsive gird layouts

--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -3,6 +3,15 @@ import style from '../style/painting.less';
 
 const Painting = () => (
   <div className={style.portfolio}>
+    <div className={style.one}/>
+    <div className={style.two}/>
+    <div className={style.three}/>
+    <div className={style.four} />
+    <div className={style.five} />
+    <div className={style.six} />
+    <div className={style.seven} />
+    <div className={style.eight} />
+    <div className={style.nine} />
   </div>
 );
 
