ae1feaf50d4fa2211dd3e044b2df213d3ccb729b
Evaline <evalinebai@gmail.com>
2018-06-21 23:58:53 -0700

    Closes #7

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,10 @@ const About = () => (
   <div>
     <div className={style.about}>
       <p>
-        I am a software developer specializing in web development, interested in finding technological solutions to public problems. I'm also a business professional, having  graduated from The University of Chicago in 2016 and gone into management consulting at Accenture. At Accenture I worked on large technology transformation for Fortune 100 companies, which gave me an initial appreciation for the ways in which tech is impacting society.
+        I am a software developer specializing in web development, interested in finding technological solutions to public problems. I also have business experience, having  graduated from The University of Chicago in 2016 and gone into management consulting at Accenture. At Accenture I worked on large technology transformations for Fortune 100 companies, which gave me an initial appreciation for the ways in which tech is impacting society.
       </p>
       <p>
-        Today, I'm busy leveling up in blockchain development, contributing to open source civic-tech projects, trying to live sustainably, and exploring beautiful Los Angeles. My favorite activities all involve creating things - be it painting, writing, coding, or cooking - and my current tools of choice are Javascript, React, Node.js, and the Ethereum stack. Perhaps the most significant thing to know about me, however, is that I am obsessed with learning new things and am always up for expanding my universe. Let's get in touch!
+        Today, I'm busy leveling up in blockchain development, contributing to civic open source projects, trying to live sustainably, and exploring beautiful Los Angeles. My favorite activities involve creating things - be it painting, writing, coding, or cooking - and my current tools of choice are Javascript, React, Node.js, and the Ethereum stack. Perhaps the most significant thing to know about me, however, is that I am obsessed with learning new things and am always up for expanding my notions of the universe. Let's get in touch!
       </p>
     </div>
   </div>
--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import style from '../style/painting.less';
 
 const Painting = () => (
-  <div className={style.art}>
-    <div className={style.one}/>
-    <div className={style.two}/>
-    <div className={style.three}/>
-    <div className={style.four} />
-    <div className={style.five} />
-    <div className={style.six} />
-    <div className={style.seven} />
-    <div className={style.eight} />
-    <div className={style.nine} />
+  <div className={style.artro}>
+    <p>
+      I like to paint when I have the time (although hard to come by) and have a fascination with portraiture. I'm available for commissions (payable in Ether) if you like rather illustrative images :)
+    </p>
+    <div className={style.art}>
+      <div className={style.one}/>
+      <div className={style.two}/>
+      <div className={style.three}/>
+      <div className={style.four} />
+      <div className={style.five} />
+      <div className={style.six} />
+    </div>
   </div>
 );
 
