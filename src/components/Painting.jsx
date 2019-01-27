2126b60ec2f4d2533597cc65f7ce296675762937
Evaline <evalinebai@gmail.com>
2019-01-26 20:26:17 -0500

    Change image hover to show painting title

--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -3,17 +3,46 @@ import style from '../style/painting.less';
 
 const Painting = () => (
   <div className={style.artro}>
-    <h3>"Wash the dust off " <em>- paraphrasing Picasso</em></h3>
     <p>
       I like to paint when I have the time and have a fascination with portraiture. I'm available for commissions if you like illustrative, hyper-literal images.
     </p>
     <div className={style.art}>
-      <div className={style.one}/>
-      <div className={style.two}/>
-      <div className={style.three}/>
-      <div className={style.four} />
-      <div className={style.five} />
-      <div className={style.six} />
+      <div className={style.one}>
+        <div className={style.title}>
+          <h3>Ceremonial Dance</h3>
+          <h5>oil on canvas</h5>
+        </div>
+      </div>
+      <div className={style.two}>
+        <div className={style.title}>
+          <h3>Pagoda</h3>
+          <h5>oil on canvas</h5>
+        </div>
+      </div>
+      <div className={style.three}>
+        <div className={style.title}>
+          <h3>Grandpa</h3>
+          <h5>acrylic on canvas</h5>
+        </div>
+      </div>
+      <div className={style.four}>
+        <div className={style.title}>
+          <h3>Migrant Mother</h3>
+          <h5>graphite on paper</h5>
+        </div>
+      </div>
+      <div className={style.five}>
+        <div className={style.title}>
+          <h3>Primarily Mao</h3>
+          <h5>acrylic on canvas</h5>
+        </div>
+      </div>
+      <div className={style.six}>
+        <div className={style.title}>
+          <h3>Sean</h3>
+          <h5>oil on canvas</h5>
+        </div>
+      </div>
     </div>
   </div>
 );
